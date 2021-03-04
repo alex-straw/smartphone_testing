0dd903db19ac10fd644b1ef2052f41b2961916ee
Alex Straw <as17163@bristol.ac.uk>
2021-03-04 15:35:57 +0000

    developing testing platform for calling processes

--- src/testing.py
+++ src/testing.py
@@ -1,3 +1,23 @@
 import numpy as np
 import cv2
+import os
 
+def get_phone_image(phone,current_path):
+    image_path = current_path + '/photographs/phone_' + str(phone) + '.jpg'
+    image = cv2.imread(image_path, 0)
+    return(image)
+
+def get_template(phone,current_path):
+    image_path = current_path + '/photographs/phone_' + str(phone) + '_template.jpg'
+    image = cv2.imread(image_path, 0)
+    return(image)
+
+def main():
+    current_path = os.path.dirname(__file__)
+    phone = 1
+
+    image = get_phone_image(phone,current_path)
+    template = get_template(phone,current_path)
+
+if __name__ == "__main__":
+    main()
